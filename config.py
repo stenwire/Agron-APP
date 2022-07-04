fdda97bd1a5220295ebd4d5000d83e566a9b5d63
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-04 22:42:35 +0100

    tweaked app for heroku

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL - done
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:despicable01@localhost:5432/genzapp'
+SQLALCHEMY_DATABASE_URI = 'postgres://zqlpsfkclvlbrx:3222e0ffd847d08da74e5bc23be4370ad5892174dc8aa4573c00938271d0345c@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d8hgqv66uofo5a'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:despicable01@localhost:5432/genzapp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
